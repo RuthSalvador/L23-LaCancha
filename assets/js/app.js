888ad2e981e7dd08325d99c9f9546afa19717508
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-19 12:36:46 -0500

    cancha y arcos

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,18 +5,20 @@ window.addEventListener("load", function(){
   cuerpo.appendChild(borde);
 
   var canchita = document.createElement("div");
+  canchita.setAttribute("class", "canchita");
   borde.appendChild(canchita);
 
   var arco1 = document.createElement("div");
+  arco1.setAttribute("class", "arco1");
   canchita.appendChild(arco1);
 
   var arco2 = document.createElement("div");
+  arco2.setAttribute("class", "arco2");
   canchita.appendChild(arco2);
 
   var centro = document.createElement("div");
+  centro.setAttribute("class", "centro");
   canchita.appendChild(centro);
-
-
 
 
 })
