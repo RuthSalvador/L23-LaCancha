436ea3dde3bcd8936a204c41fa28f97d78e1f9ee
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-19 12:02:45 -0500

    estructura inicial js

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function(){
+  var cuerpo = document.getElementById("body");
+
+  var borde = document.createElement("section");
+  cuerpo.appendChild(borde);
+
+  var canchita = document.createElement("div");
+  borde.appendChild(canchita);
+
+  var arco1 = document.createElement("div");
+  canchita.appendChild(arco1);
+
+  var arco2 = document.createElement("div");
+  canchita.appendChild(arco2);
+
+  var centro = document.createElement("div");
+  canchita.appendChild(centro);
+
+
+
+
+})
